d4e6cc8e2d14048761e7d98e5812072ab7e984de
NateDogg1232 <nathanielbarragan@gmail.com>
2018-02-04 11:37:43 -0800

    Added a map loader I also added a way to check for some keys that are pressed on the keyboard.

--- program/main.py
+++ program/main.py
@@ -18,7 +18,7 @@ class Tileset:
         #Iterates through the image, pulling out tiles at the width and height passed
         for tile_x in range(0, image_width/tile_width):
             line = []
-            tile_table.append(line)
+            self.tile_table.append(line)
             for tile_y in range(0, image_height/tile_height):
                 rect = (tile_x * tile_width, tile_y * tile_height, tile_width, tile_height)
                 line.append(image.subsurface(rect))
@@ -36,6 +36,33 @@ class Player(Sprite):
         super(Sprite, self).__init__(file)
 
 
+class Map:
+
+    #We can load our map here
+    def __init__(self, mapname):
+        self.mapname = mapname
+        self.map = []
+        self.key = {}
+        parser = ConfigParser.ConfigParser()
+        parser.read("assets/maps/" + mapname + ".map")
+        self.tileset = Tileset(("assets/tilesets/" + parser.get("level", "tileset")), int(parser.get("level", "tilewidth")), int(parser.get("level", "tileheight")))
+        self.map = parser.get("level", "map").split('\n')
+        for section in parser.sections():
+            if len(section) == 1:
+                desc = dict(parser.items(section))
+                self.key[section] = desc
+        self.width = len(self.map[0])
+        self.height = len(self.map)
+
+    def getTile(self, x, y):
+        try:
+            char = self.map[y][x]
+        except IndexError:
+            return {}
+        try:
+            return self.key[char]
+        except KeyError:
+            return {}
 
 #For the game we use a simple state machine
 class Game:
@@ -43,10 +70,13 @@ class Game:
     def __init__(self):
         # The default state can be the title menu
         self.state = 0
-        self.mapTileSet = Tileset()
-    def loadMap(self, file):
-        #Placeholder
-        return None
+        self.currentLevel = None
+
+    def loadOverworldMap(self, mapname):
+        #We set the state to overworld
+        self.state = 10
+        self.currentLevel = Map(mapname)
+
 
 
 
@@ -54,7 +84,22 @@ if __name__=='__main__':
     #Initalize the pygame library
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(SCREEN_DEFAULT_COLOR)
-    pygame.display.flip()
     #Set the current state to the overworld
-    Game.state = 10
+    game = Game()
+    game.loadOverworldMap("testmap")
+
+    #We create an infinite loop
+    while 1:
+        #We check for any events that may have occured
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                SCREEN_DEFAULT_COLOR = (255,0,0)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    SCREEN_DEFAULT_COLOR = (255,255,255)
+        screen.fill(SCREEN_DEFAULT_COLOR)
+        pygame.display.flip()
+        pygame.time.wait(1)
