73378bf7c92f1e863158df646ee8c96ff26c0c01
Sean Raymond <carbomechgaming@gmail.com>
2018-02-05 20:31:47 -0800

    Added the p5 library and basic notes for the map generator.

---
+++ program/map-generator.js
@@ -0,0 +1,13 @@
+//Notes for the code necessary for the generator as well as necessary features
+    //We need it to output valid config files given in the example file in the maps file in assets
+
+
+
+    //Randomly generate map patterns
+        //In a pattern of #s and .s
+            //#s are walls and .s are floor
+
+
+function mapGenerate() {
+
+}
